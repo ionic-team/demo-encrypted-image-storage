590f0fbd9fcb8523de8f5be0f78d8ed82b733d97
Matt Netkow <matt.netkow@gmail.com>
2019-11-07 16:25:11 -0600

    finalize main demo loop

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { WebView } from '@ionic-native/ionic-webview/ngx';
 import { ImageService } from '../services/image.service';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -9,17 +11,31 @@ import { ImageService } from '../services/image.service';
 export class HomePage {
   public safeImg;
 
-  constructor(public imageService: ImageService) {
-    
+  constructor(public imageService: ImageService, private webview: WebView, private platform: Platform) {
   }
 
-  ngOnInit() {
-
+  async ngOnInit() {
+    const secureImage = await this.imageService.getSecurelyStoredImage();
+    if (secureImage !== null) {
+      console.log("got saved image");
+      this.safeImg = secureImage;
+    }
+    else {
+      if (this.platform.is("cordova")) {
+        this.safeImg = this.webview.convertFileSrc("assets/image-placeholder.jpg");
+      } else {
+        this.safeImg = "assets/image-placeholder.jpg";
+      }
+    }
   }
 
-  async loadImage() {
+  async captureImage() {
+    let image = await this.imageService.captureNewImage();
+    this.safeImg = image;
+  }
+
+  async loadImageFromSecureStorage() {
     let image = await this.imageService.getImageUsingObjectUrl();
-    console.log("setting image to display");
     this.safeImg = image;
   }
 }
--- src/app/services/image.service.ts
+++ src/app/services/image.service.ts
@@ -25,19 +25,24 @@ import { IdentityService } from '../services/identity.service';
 export class ImageService {
   private database: Database;
   private readyPromise: Promise<void>;
-  private savedDoc: MutableDocument;
+
+  private DOC_NAME = "docSecureImage";
+  private DOC_BLOB_NAME = "blobSecureImage";
+  private DATABASE_NAME = "secureImageStorage";
   
   constructor(private camera: Camera, private file: File, private webview: WebView, private sanitizer: DomSanitizer, 
     private identityService: IdentityService) { 
     this.readyPromise = this.initializeDatabase();
   }
 
-  public async saveImage() {
+  public async captureNewImage() {
     const options: CameraOptions = {
-      quality: 10,
+      quality: 50,
       destinationType: this.camera.DestinationType.FILE_URI,
       encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE
+      mediaType: this.camera.MediaType.PICTURE,
+      targetWidth: 600,
+      targetHeight: 400
     }
     
     const capturedTempImage = await this.camera.getPicture(options);
@@ -48,12 +53,11 @@ export class ImageService {
         const fileReader = new FileReader();
         fileReader.onloadend = () => {
           let blob = new Blob("image/jpeg", fileReader.result as ArrayBuffer);
-          this.savedDoc = new MutableDocument();
-          this.savedDoc.setBlob("test", blob);
+          const imageDoc = new MutableDocument(this.DOC_NAME);
+          imageDoc.setBlob(this.DOC_BLOB_NAME, blob);
 
           try {
-            this.database.save(this.savedDoc);
-            console.log("saved doc");
+            this.database.save(imageDoc);
           } catch (err) {
             console.error(err);
           }
@@ -64,17 +68,31 @@ export class ImageService {
     } catch(err) {
       console.log(err);
     }
+
+    // Display the image immediately. Held in temporary storage, so the OS will clear 
+    // out eventually. When the user loads the app at a later time, the encrypted image 
+    // is retrieved securely from Offline Storage instead.
+    const resolvedImg = this.webview.convertFileSrc(capturedTempImage);
+    return this.sanitizer.bypassSecurityTrustUrl(resolvedImg);
   }
 
-  public getImage() {
+  // Retrieve the encrypted image from Offline Storage then display as a 
+  // base64 image stream
+  public async getSecurelyStoredImage() {
+    await this.readyPromise;
+
+    const imageDoc = await this.database.getDocument(this.DOC_NAME);
+    if (imageDoc === null) {
+      return null;
+    }
+
     return new Promise((resolve, reject) => {
-      this.savedDoc.getBlobContent("test", this.database).then((docBlob) => {
+      imageDoc.getBlobContent(this.DOC_BLOB_NAME, this.database).then((docBlob) => {
         var bytes = new Uint8Array(docBlob);
         var blob = new window.Blob([bytes.buffer], { type: "image/jpeg"});
 
         var reader = new FileReader();
         reader.onloadend = () => {
-          console.log("finished loading image. Length: " + (reader.result as string).length);
           resolve(reader.result);
         };
         reader.readAsDataURL(blob);
@@ -82,20 +100,33 @@ export class ImageService {
     });
   }
 
+  // Retrieve the encrypted image from Offline Storage then display as a 
+  // object URL
   public async getImageUsingObjectUrl() {
-    let docBlob = await this.savedDoc.getBlobContent("test", this.database);
+    const imageDoc = await this.database.getDocument(this.DOC_NAME);
+    if (imageDoc === null) {
+      console.log("image not found");
+      return null;
+    }
+
+    let docBlob = await imageDoc.getBlobContent(this.DOC_BLOB_NAME, this.database);
     var arrayBufferView = new Uint8Array(docBlob);
     var blob = new window.Blob([ arrayBufferView.buffer ], { type: "image/jpeg"});
     var objectUrl = window.URL.createObjectURL(blob);
+    console.log("image found: " + objectUrl);
     return this.sanitizer.bypassSecurityTrustUrl(objectUrl);
   }
 
+  // Initialize the Offline Storage Couchbase Lite database
+  // Use Identity Vault to create/get an encryption key unique to the app user.
   private async initializeDatabase(): Promise<void> {
     return new Promise(resolve => {
       IonicCBL.onReady(async () => {
+        // Get (or create) a unique encryption key per user
         const config = new DatabaseConfiguration();
         config.setEncryptionKey(await this.identityService.getEncryptionKey());
-        this.database = new Database('secureImageStorage', config);
+
+        this.database = new Database(this.DATABASE_NAME, config);
         this.database.setEngine(
           new CordovaEngine({
             allResultsChunkSize: 9999
